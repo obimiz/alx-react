6807815da0fff4511621c425c80aecbf5e220b55
obimiz <mbanuobinna.com>
2024-08-30 13:49:27 +0100

    Create basic tests with four tests

--- 0x01-react_intro/task_0/dashboard/src/App.js
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -1,6 +1,9 @@
 import logo from './Holberton-logo.jpg';
 import './App.css';
 import './index.css'
+import { getFullYear, getFooterCopy } from "./utils";
+import Notifications from './Notifications';
+
 
 function App() {
   return (
@@ -9,18 +12,33 @@ function App() {
       <img src={logo} alt="Holberton logo" />
       <h1>School dashboard</h1>
      </div>
+      <Notifications />
 
      <hr />
 
       <div className="App-body">
         <p>Login to access the full dashboard</p>
 
+        {/* Form for email and password */}
+        <form>
+          <div>
+            <label htmlFor='email'>Email:</label>
+            <input type='email' id='email' name='email' />
+          </div>
+
+          <div>
+            <label htmlFor='password'>Password:</label>
+            <input type='password' id='password' name='password' />
+          </div>
+          <button type='submit'>Ok</button>
+        </form>
+
       </div>
 
       <hr />
 
       <footer className="App-footer">
-        <p>Copyright 2020 - holberton School</p>
+      Copyright {getFullYear()} - {getFooterCopy()}
       </footer>
 
 
--- 0x01-react_intro/task_0/dashboard/src/index.js
+++ 0x01-react_intro/task_0/dashboard/src/index.js
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Notifications from './Notifications';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+  </React.StrictMode>
+);
+
+const rootNotifications = ReactDOM.createRoot(document.getElementById("root-notifications"));
+rootNotifications.render(
+  <React.StrictMode>
+    <Notifications />
   </React.StrictMode>
 );
 
