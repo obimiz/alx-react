0446cc55c5e4f7917fa735b2e5b82f22f84de0f7
obimiz <mbanuobinna.com>
2024-09-13 21:27:42 +0100

    List and Push

--- 0x06-React_Immutable/3-list.js
+++ 0x06-React_Immutable/3-list.js
@@ -1,5 +1,4 @@
 import { List } from 'immutable';
-import { appendToMemberExpression } from '../../.cache/typescript/5.5/node_modules/@babel/types/lib/index';
 
 export function getListObject(array) {
 	return List(array)
